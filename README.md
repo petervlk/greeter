1b3e1d2ec7a9a7e96ec1e42650baaec2e33c8f57
Peter Vlk <petervlk86@gmail.com>
2023-12-10 01:54:12 +0100

    add basic jetty setup

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # petervlk/greeter
 
-FIXME: my new application.
+This repository contains `Hello, World!` application meant to be used as a example/study material
+for https://github.com/weavejester/integrant setup and usage.
 
 ## Installation
 
@@ -8,5 +9,8 @@ Download from https://github.com/petervlk/greeter
 
 ## Usage
 
-FIXME: explanation
+Run the project directly, via `:main-opts` (`-m greeter.core`):
 
+    $ clojure -M:run-m
+
+Visit URL http://localhost:9010/
--- src/greeter/core.clj
+++ src/greeter/core.clj
@@ -1,11 +1,39 @@
 (ns greeter.core
+  (:require [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
 (defn greet
-  "Callable entry point to the application."
   [name]
   (format "Hello, %s!" (or name "World")) )
 
+(defn handler
+  [_req]
+  {:status 200
+   :body   (greet "Jack")})
+
+(defonce server (atom nil))
+
+(defn server-start
+  []
+  (when-not @server
+    (reset! server (run-jetty #'handler {:port 9010 :join? false}))))
+
+(defn server-stop
+  []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+(defn server-restart
+  []
+  (server-stop)
+  (server-start))
+
+(comment
+  (server-restart)
+
+  *e)
+
 (defn -main
-  [& args]
-  (println (greet (first args))))
+  [& _args]
+  (server-start))
